02124f22a28b06db88c2424272a2a87ea0631c3f
Quoc-99 <gitquoc99@gmail.com>
2020-07-06 10:59:26 +0700

    first

---
+++ app/src/main/java/com/example/exercise_2/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.exercise_2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+
+            val intent = Intent(this, Activity_1::class.java)
+
+            startActivity(intent)
+        }
+    }
+}
